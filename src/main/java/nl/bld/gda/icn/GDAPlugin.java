6055f5e35c87bfbcb5f8091b00f831c3ea308cc2
Ramesh R. Bhat <bhatr00@ont.belastingdienst.nl>
2025-01-15 09:35:37 +0100

    initial_commit

---
+++ src/main/java/nl/bld/gda/icn/GDAPlugin.java
@@ -0,0 +1,29 @@
+package nl.bld.gda.icn;
+import java.util.Locale;
+import com.ibm.ecm.extension.Plugin;
+import com.ibm.ecm.extension.PluginAction;
+
+public class GDAPlugin extends Plugin{
+	@Override
+	public String getId() {
+		return "GDAPlugin";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		// TODO Auto-generated method stub
+		return "GDAPlugin";
+	}
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return "1.0";
+	}
+
+	@Override
+	public String getScript() {
+		return "GDAPlugin.js";
+	}
+
+}
---
+++ src/main/webapp/GDAPlugin.js
@@ -0,0 +1,8 @@
+console.log("Getting inside the GDAPlugin Scope");
+
+require(["dojo/_base/declare", "dojo/_base/lang"], function(declare, lang) {
+  
+        //Use this function to add any global JavaScript methods your plug-in requires.
+
+		console.log("GDAPlugin Scope - End");
+	});
